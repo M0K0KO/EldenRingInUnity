f925e907b406f8a2a689ff16a6e923f8c13595fd
M0K0KO <kevin8193@naver.com>
2024-08-20 18:08:21 +0900

    EPISODE 3

--- Assets/Scripts/Character/Player/PlayerInputManager.cs
+++ Assets/Scripts/Character/Player/PlayerInputManager.cs
@@ -12,7 +12,10 @@ namespace SG
 
         PlayerControls playerControls;
 
-        [SerializeField] Vector2 movement;
+        [SerializeField] Vector2 movementInput;
+        public float verticalInput;
+        public float horizontalInput;
+        public float moveAmount;
 
         private void Awake()
         {
@@ -58,7 +61,7 @@ namespace SG
             {
                 playerControls = new PlayerControls();
 
-                playerControls.PlayerMovement.Movement.performed += i => movement = i.ReadValue<Vector2>();
+                playerControls.PlayerMovement.Movement.performed += i => movementInput = i.ReadValue<Vector2>();
             }
 
             playerControls.Enable();
@@ -68,6 +71,43 @@ namespace SG
         {
             // 이 OBJECT를 삭제하면, EVENT로부터 DETACH
             SceneManager.activeSceneChanged -= OnSceneChange;
+        }
+
+        private void OnApplicationFocus(bool focus)
+        {
+            if (enabled)
+            {
+                if (focus)
+                {
+                    playerControls.Enable();
+                }
+                else
+                {
+                    playerControls.Disable();
+                }
+            }
+        }
+
+        private void Update()
+        {
+            HandleMovementInput();
+        }
+
+        private void HandleMovementInput()
+        {
+            verticalInput = movementInput.y;
+            horizontalInput = movementInput.x;
+
+            moveAmount = Mathf.Clamp01(Mathf.Abs(verticalInput) + Mathf.Abs(horizontalInput));
+
+            if (moveAmount <= 0.5 && moveAmount > 0) 
+            {
+                moveAmount = 0.5f;
+            }
+            else if (moveAmount > 0.5 && moveAmount <= 1)
+            {
+                moveAmount = 1;
+            }
         }
     }
 }
--- Assets/Scripts/Character/Player/PlayerManager.cs
+++ Assets/Scripts/Character/Player/PlayerManager.cs
@@ -2,17 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+namespace SG
+{
+    public class PlayerManager : CharacterManager
     {
-        
+        PlayerLocomotionManager playerLocomotionManager;
+        protected override void Awake()
+        {
+            base.Awake();
+
+            playerLocomotionManager = GetComponent<PlayerLocomotionManager>();
+        }
+
+        protected override void Update()
+        {
+            base.Update();
+
+            // GAMEOBJECT의 소유주가 아니면 접근불가하도록 설정
+            if (!IsOwner)
+                return;
+
+            // 모든 움직인 관리
+            playerLocomotionManager.HandleAllMovement();
+        }
     }
 }
